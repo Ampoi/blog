2c3051f007ee76985cad87e5f432eb98859d8a9c
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-01-03 17:14:51 +0900

    Blogの追加

--- app.vue
+++ app.vue
@@ -1,29 +1,9 @@
 <template>
-  <div class="relative snap-y snap-mandatory overflow-y-auto h-screen">
-    <div class="snap-center h-screen w-screen grid place-content-center">
-      <div>
-        <h1 class="text-8xl text-center font-['Josefin_Sans']">Ampoi</h1>
-        <div class="mt-2 flex flex-row gap-2 justify-center">
-          <NuxtLink to="https://twitter.com/4mpoi">
-            <img src="@/assets/sns_logos/twitter.svg" alt="Twitter" class="h-5">
-          </NuxtLink>
-          <NuxtLink to="https://github.com/Ampoi">
-            <img src="@/assets/sns_logos/github.svg" alt="Github" class="h-5">
-          </NuxtLink>
-        </div>
-      </div>
-    </div>
-    <div class="snap-center h-screen w-screen grid place-content-center">
-      <div class="w-screen px-10">
-        <div>
-          <h1 class="text-6xl font-bold -ml-4 text-black/20">01</h1>
-          <h1 class="text-5xl font-bold -mt-8">作字</h1>
-        </div>
-        <p class="mt-2 text-lg">字のデザインをしたりとかしてます。</p>
-      </div>
-    </div>
-  </div>
+  <NuxtPage/>
 </template>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap');
+body {
+  background-color: #e2e8f0;
+}
 </style>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="relative snap-y snap-mandatory overflow-y-auto h-screen">
+    <div class="snap-center h-screen w-screen grid place-content-center">
+      <div>
+        <h1 class="text-8xl text-center font-['Josefin_Sans']">Ampoi</h1>
+        <div class="mt-2 flex flex-row gap-2 justify-center">
+          <NuxtLink to="https://twitter.com/4mpoi">
+            <img src="@/assets/sns_logos/twitter.svg" alt="Twitter" class="h-5">
+          </NuxtLink>
+          <NuxtLink to="https://github.com/Ampoi">
+            <img src="@/assets/sns_logos/github.svg" alt="Github" class="h-5">
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+    <div class="snap-center h-screen w-screen grid place-content-center">
+      <div class="max-w-2xl px-10">
+        <div>
+          <h1 class="text-6xl font-bold -ml-4 text-black/20">01</h1>
+          <h1 class="text-5xl font-bold -mt-8">作字</h1>
+        </div>
+        <p class="mt-2 text-lg">字のデザインをしたりとかしてます。</p>
+      </div>
+    </div>
+    <div class="snap-center h-screen w-screen grid place-content-center">
+      <div class="max-w-2xl px-10">
+        <div>
+          <h1 class="text-6xl font-bold -ml-4 text-black/20">02</h1>
+          <h1 class="text-5xl font-bold -mt-8">Blog</h1>
+        </div>
+        <p class="mt-2 text-lg">ブログです。micro-cmsで管理してます。</p>
+        <div class="mt-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div
+            v-for="blog in blogs"
+            :key="blog.key"
+            class="flex flex-col bg-white/40 rounded-md"
+          >
+            <div :style="`background-image: url(${blog.eyecatch.url});`" class="basis-40 bg-cover rounded-t-md"></div>
+            <h2 class="p-4 pt-2 text-xl font-bold border-2 border-t-0 rounded-md border-white/40">{{blog.title}}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import  { createClient } from "microcms-js-sdk"
+
+export default {
+  data(){return{
+    blogs: [],
+    client: createClient({
+      serviceDomain: "ampoi-blog",
+      apiKey: useRuntimeConfig().public.apikey
+    })
+  }},
+  mounted(){
+    this.client
+      .get({
+        endpoint: 'blogs'
+      })
+      .then((res) => {
+        this.blogs = res.contents
+      });
+  }
+}
+</script>
